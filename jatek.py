22aa12fbcfabf7f2928c6d97c4bc8eba459f0d24
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2023-04-07 16:45:51 +0200

    Színek tesztelgetése

--- jatek.py
+++ jatek.py
@@ -1,14 +1,20 @@
 import pyfiglet
-from rich import print
+from termcolor import colored
+
+'''from rich import print
 from rich.prompt import Prompt
 from rich.table import Table
-from rich.console import Console
+from rich.console import Console'''
   
-bannerKep = pyfiglet.figlet_format("Kalandprogram", font = "slant")
-print("\n", bannerKep)
+print("") 
 
-print("")
-print("[green]|[/green]  scientifically accurate, survival text-based adventure\nset in the future where climate change is inevitable  [green]|[/green]")
+bannerKep = pyfiglet.figlet_format("Kalandprogram", font = "slant") # , font = "slant"
+# pyfiglet.figlet_format("Kalandprogram", font = "banner3-D" )
+print(colored(bannerKep, 'blue'))
+print(bannerKep)
+
+print(colored("| ", 'green'), "scientifically accurate, survival text-based adventure\nset in the future where climate change is inevitabl", colored(" |", 'green'))
+
 print(" ")
 
 print("[bold red]Welcome to Lost Cause.\nWould you like to load a save or start a new game?")
