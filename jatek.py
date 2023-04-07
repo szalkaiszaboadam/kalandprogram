568e1ba117c88d8f58f768c42527cb1cd0228450
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2023-04-07 17:15:08 +0200

    Input

--- jatek.py
+++ jatek.py
@@ -8,14 +8,17 @@ from rich.console import Console'''
   
 print("") 
 
-#bannerKep = pyfiglet.figlet_format("Kalandprogram", font = "slant") # , font = "slant"
-bannerKep = pyfiglet.figlet_format("Kalandprogram", font = "banner3-D" )
-# print(colored(bannerKep, 'blue'))
-print(bannerKep)
+bannerKep = pyfiglet.figlet_format("Kalandprogram", font = "slant") # , font = "slant"
+# bannerKep = pyfiglet.figlet_format("Kalandprogram", font = "banner3-D" )
+print(colored(bannerKep, 'light_grey'))
+# print(bannerKep)
 
-print(colored("| ", 'green'), "scientifically accurate, survival text-based adventure\nset in the future where climate change is inevitabl", colored(" |", 'green'))
+print(colored("| ", 'green') + "scientifically accurate, survival text-based adventure\nset in the future where climate change is inevitabl", colored(" |", 'green'))
 print(" ")
 print(colored("Welcome to Lost Cause.\nWould you like to load a save or start a new game?", 'red',  attrs=['bold']))
 print(" ")
-print(colored(" [L]", 'yellow',  attrs=['bold']), "LOAD")
-print(colored(" [N]", 'yellow',  attrs=['bold']), "NEW GAME")
+print(colored(" [L] ", 'yellow',  attrs=['bold']) + "LOAD")
+print(colored(" [N] ", 'yellow',  attrs=['bold']) + "NEW GAME")
+
+elsoinput = input("---> " + colored("[N/L]", 'magenta') + ": ")
+
